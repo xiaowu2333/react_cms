2db21fc50b1e97d320862b844e7daa3962f54b69
xiaowu <xiaowu123xw@gmail.com>
2020-03-09 01:36:02 +0800

    路由配置按需加载

--- src/router/index.js
+++ src/router/index.js
@@ -4,18 +4,45 @@
  * @Author: xiaowu
  * @Date: 2020-03-06 16:23:34
  * @LastEditors: xiaowu
- * @LastEditTime: 2020-03-08 20:19:53
+ * @LastEditTime: 2020-03-09 01:30:12
  */
-import Home from "../App";
-import Login from "../components/login";
-import Register from "../components/register";
+import React from 'react'
+import Loadable from 'react-loadable'
 
-import Dashboard from "../components/dashboard";
-import List from "../components/list";
-import Message from "../components/message";
-import Setting from "../components/setting";
+const Home = Loadable({
+    loader: () => import('../App'),
+    loading: ()=><div>loadding...</div>,
+});
 
+const Login = Loadable({
+    loader: () => import('../components/login'),
+    loading: ()=><div>loadding...</div>,
+});
 
+const Register = Loadable({
+    loader: () => import('../components/register'),
+    loading: ()=><div>loadding...</div>,
+});
+
+const Dashboard = Loadable({
+    loader: () => import('../components/dashboard'),
+    loading: ()=><div>loadding...</div>,
+});
+
+const List = Loadable({
+    loader: () => import('../components/list'),
+    loading: ()=><div>loadding...</div>,
+});
+
+const Message = Loadable({
+    loader: () => import('../components/message'),
+    loading: ()=><div>loadding...</div>,
+});
+
+const Setting = Loadable({
+    loader: () => import('../components/setting'),
+    loading: ()=><div>loadding...</div>,
+});
 
 
 export const routerList = [
